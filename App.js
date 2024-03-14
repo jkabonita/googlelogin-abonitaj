cdd3275af3df87cdfe7472f2f89cb0145fb113d8
jkabonita <122688280+jkabonita@users.noreply.github.com>
2024-03-15 00:53:40 +0800

    Part 6 Google Login No Firebase

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+//expo go: 461279521695-d950a0pe0u67uk9gg0lugqrp7rlhsh5d.apps.googleusercontent.com
+//android: 461279521695-70qgvnsnut382het7sqou9mguq53lv4j.apps.googleusercontent.com
+//ios: 461279521695-18bapf3g14uakj0thprbkf8eh7g51blq.apps.googleusercontent.com
+//web: 461279521695-5cd784mqptv60d4jrba2fhp4v5eorinu.apps.googleusercontent.com
 export default function App() {
   return (
     <View style={styles.container}>
